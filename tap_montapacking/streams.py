8d3044e4e2c95a38229df74ab55812559eed562f
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2023-11-16 10:49:26 -0300

    Rollback checking on PoNumbers being numeric

--- tap_montapacking/streams.py
+++ tap_montapacking/streams.py
@@ -217,7 +217,7 @@ class InboundsForecastParentStream(MontapackingStream):
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         if "No groups found for these filters" in record.get("Message", ""):
             return None
-        if self.last_child == record["PoNumber"] or not record["PoNumber"].isnumeric():
+        if self.last_child == record["PoNumber"]:
             return None
         self.last_child = record["PoNumber"]
         return {"id": record["PoNumber"]}
