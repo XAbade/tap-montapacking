9165181f1ca43ec08fbf1da3944cd097ab34229b
Key <keynarafaelp@gmail.com>
2024-06-19 15:43:15 -0500

    not sync forecasts with empty reference

--- tap_montapacking/streams.py
+++ tap_montapacking/streams.py
@@ -237,7 +237,7 @@ class InboundsForecastParentStream(MontapackingStream):
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         if "No groups found for these filters" in record.get("Message", ""):
             return None
-        if not record["Reference"]:
+        if not record.get("Reference"):
             return None
         if self.last_child == record["Reference"]:
             return None
